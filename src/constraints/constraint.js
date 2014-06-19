d0109989e71a1493c466056565d46fa492e21596
Tim Shelburne <shelburt02@gmail.com>
2014-06-19 17:18:57 -0500

    rewrote and tested the new base constraint

---
+++ src/constraints/constraint.js
@@ -0,0 +1,35 @@
+define(function() {
+
+	/**
+	 * HELPERS
+	 */
+
+	var isFunc = function(obj) {
+		return obj instanceof Function;
+	};
+
+	/**
+	 * MODULE
+	 */
+
+	var Constraint = function(type, check, error) {
+		if (!(typeof(type) === 'string')) {
+			throw new TypeError('Only strings may be used for the constraint type.');
+		}
+
+		this.type  = type;
+		this.check = check;
+		this.error = error || 'There is a problem with your input.';
+	};
+
+	Constraint.prototype.fails = function(element) {
+		return element.getAttribute('type') === this.type && !this.check(element);
+	};
+
+	Constraint.prototype.errorMessage = function(element) {
+		return isFunc(this.error) ? this.error(element) : this.error;
+	};
+
+	return Constraint;
+
+});
---
+++ test/constraints_spec.js
@@ -0,0 +1,61 @@
+define(function(require) {
+
+	var getInput = function(type, value) {
+		var input = document.createElement('input');
+		input.type = type || 'creditcard';
+		input.value = value || '4111111111111111';
+		return input;
+	}
+
+	describe('the base constraint', function() {
+
+		var Constraint = require('constraints/constraint');
+
+		it('throws without a string type', function() {
+			expect(function() { new Constraint(true); }).toThrow();
+		});
+
+		it('does not fail when the type does not match', function() {
+			var constraint = new Constraint('nomatch');
+			expect(constraint.fails(getInput())).toBeFalsy();
+		});
+
+		it('throws when the type matches and no check has been given', function() {
+			var constraint = new Constraint('match');
+			expect(function() { constraint.fails(getInput('match')); }).toThrow();
+		});
+
+		it('fails when the type matches and the check fails', function() {
+			var constraint = new Constraint('match', function(element) { 
+				return false; 
+			});
+			expect(constraint.fails(getInput('match'))).toBeTruthy();
+		});
+
+		it('does not fail when the type matches and the check passes', function() {
+			var constraint = new Constraint('match', function(element) {
+				return true;
+			});
+			expect(constraint.fails(getInput('match'))).toBeFalsy();
+		});
+
+		it('returns a default failure message', function() {
+			var constraint = new Constraint('match');
+			expect(constraint.errorMessage(getInput('match'))).toEqual('There is a problem with your input.');
+		});
+
+		it('returns a specified string for the failure message', function() {
+			var constraint = new Constraint('match', null, 'A failure occurred.');
+			expect(constraint.errorMessage(getInput('match'))).toEqual('A failure occurred.');
+		});
+
+		it('returns the result of a specified function for the error message', function() {
+			var constraint = new Constraint('match', null, function(element) {
+				return 'An element of type \'' + element.getAttribute('type') + '\' doesn\'t work.';
+			});
+			expect(constraint.errorMessage(getInput('match'))).toEqual('An element of type \'match\' doesn\'t work.');
+		});
+
+	});
+
+});
