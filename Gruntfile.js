56a80179e1de6c7e607f8d99f3e6a045f123fec4
Tim Shelburne <shelburt02@gmail.com>
2014-06-21 13:58:56 -0500

    added requirejs grunt configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,29 @@
 module.exports = function(grunt) {
 
-
-
-  // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
     jshint: {
       files: [ 'Gruntfile.js', 'src/**/*.js', 'test/**/*.js' ]
+    },
+
+    requirejs: {
+      compile: {
+        options: {
+          baseUrl: "src",
+          optimize: 'none',
+          name: "knowitall", 
+          insertRequire: [ 'knowitall' ],
+          out: "build/knowitall.js"
+        }
+      }
     }
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
 
-  // Default task(s).
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'requirejs']);
 
 };
