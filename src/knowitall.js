cd23fc7a44178c376c855fbe8fe411208840c04f
Tim Shelburne <shelburt02@gmail.com>
2014-07-14 20:28:01 -0500

    wrote tests around novalidate spec for library interface

--- src/knowitall.js
+++ src/knowitall.js
@@ -1,29 +1,93 @@
 define(function(require) {
 
 	var Validator = require('validator');
+	var Constraint = require('constraints/constraint');
 	var CreditCardConstraint = require('constraints/credit_card_constraint');
 	var EventUtil = require('util/events');
-	
+	var FormUtil = require('util/forms');
+
 	// Set up native validator
-	var validator = new Validator();
-	validator.register(new CreditCardConstraint());
+	var _validator = new Validator();
+	_validator.register(new CreditCardConstraint());
 
-	// Handles validating and re-submitting the form
-	var submitForm = function(e) {
+	var _shouldValidate = function(form, submit) {
+		return !FormUtil.noValidate(form, submit);
+	};
+
+	// Handles validating when a form submit click event is fired
+	var _handleClick = function(e) {
 		e = e || event;
-		e.preventDefault();
-
-		form = e.target || e.srcElement;
-		if (validator.validateForm(form)) {
-			form.submit();
+		var submit = e.target || e.srcElement;
+		var form = submit.form;
+		
+		if (_shouldValidate(form, submit)) {
+			_validator.validateForm(form);
 		}
 	};
 
-	// Automatically binds to all forms on the page
+	// Handles validating and re-submitting the form when a form submit event is fired
+	var _handleSubmit = function(e) {
+		e = e || event;
+		var form = e.target || e.srcElement;
+		var submit = FormUtil.activeSubmit(form);
+		
+		if (_shouldValidate(form, submit) && !_validator.validateForm(form)) {
+			// prevent the browser from taking action
+			e.preventDefault();
+			// prevent ancestors from taking action
+			e.stopPropagation();
+			// prevent sibling listeners from taking action
+			e.stopImmediatePropagation();
+			submit.click();
+		}
+	};
+
+	/**
+	 * KnowItAll object
+	 * An instance of _KnowItAll is the exclusive interface for developers
+	 */
+	var _KnowItAll = function() {
+		this.handledForms = [];
+	};
+
+	// Registers a form to be auto-validated on submit
+	_KnowItAll.prototype.registerForm = function(form) {
+		if (!this.hasForm(form)) {
+			EventUtil.addEventHandler(form, 'submit', _handleSubmit);
+			FormUtil.submits(form).forEach(function(submit) { 
+				EventUtil.addEventHandler(submit, 'click', _handleClick);
+			});
+			this.handledForms.push(form);
+		}
+	};
+
+	// Deregisters auto-validation of forms on submit
+	_KnowItAll.prototype.deregisterForm = function(form) {
+		if (this.hasForm(form)) {
+			EventUtil.removeEventHandler(form, 'submit', _handleSubmit);
+			FormUtil.submits(form).forEach(function(submit) { 
+				EventUtil.removeEventHandler(submit, 'click', _handleClick);
+			});
+			this.handledForms.splice(this.handledForms.indexOf(form), 1);
+		}
+	};
+
+	// Checks whether the given form is currently registered
+	_KnowItAll.prototype.hasForm = function(form) {
+		return this.handledForms.indexOf(form) >= 0;
+	};
+
+	// Allows registering a custom constraint with the validator
+	_KnowItAll.prototype.registerConstraint = function(type, check, error) {
+		_validator.register(new Constraint(type, check, error));
+	};
+
+	var knowItAll = new _KnowItAll();
+
 	Array.prototype.forEach.call(document.forms, function(form) {
-		EventSupport.addEventHandler(form, 'submit', submitForm);
+		knowItAll.registerForm(form);
 	});
 
-	return validator;
+	return knowItAll;
 
 });
