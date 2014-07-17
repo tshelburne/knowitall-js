46585c0cef063caf6564aaabfeddca8048534924
Tim Shelburne <shelburt02@gmail.com>
2014-07-17 09:14:45 -0500

    added input builder support for creating inputs with various attributes

--- test/support/builder.js
+++ test/support/builder.js
@@ -3,9 +3,16 @@ define(function() {
 	return {
 
 		buildInput: function(type, value, attrs) {
+			attrs = attrs || {};
+			
 			var input = document.createElement('input');
 			input.setAttribute('type', type || 'text');
 			input.setAttribute('value', value || 'test value');
+			
+			Object.keys(attrs).reduce(function(acc, key) {
+				return acc.setAttribute(key, attrs[key]);
+			}, input);
+
 			return input;
 		},
 
